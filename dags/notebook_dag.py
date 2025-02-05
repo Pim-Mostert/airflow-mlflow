1b0f098d6c63e40cb3a3872796470348da300c61
Pim Mostert <mostert.pim@gmail.com>
2025-02-05 21:37:31 +0100

    Got something working with tempfile

--- dags/notebook_dag.py
+++ dags/notebook_dag.py
@@ -1,30 +1,43 @@
 import os
+import tempfile
 from airflow import DAG
-from airflow.providers.papermill.operators.papermill import PapermillOperator
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+import nbformat
+from nbconvert import HTMLExporter
+import papermill as pm
 
 INPUT_DIR = os.path.realpath(__file__)
 OUTPUT_DIR = "/tmp"
 FILE_NAME = "notebook.ipynb"
 
+
+def execute_notebook_and_convert(**kwargs):
+    # Read the input notebook
+    INPUT_FILE = os.path.join(os.path.dirname(INPUT_DIR), FILE_NAME)
+
+    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fp:
+        pm.execute_notebook(
+            INPUT_FILE,
+            fp.name,
+        )
+
+        # Read notebook
+        executed_notebook = nbformat.read(fp, as_version=4)
+
+    # Convert notebook to HTML
+    html_exporter = HTMLExporter()
+    (html_body, _) = html_exporter.from_notebook_node(executed_notebook)
+
+    # Save the final HTML to disk
+    with open(os.path.join(OUTPUT_DIR, "output.html"), "w") as f:
+        f.write(html_body)
+
+
 with DAG(
     dag_id="run_notebook",
     description="Run notebook and export HTML",
 ) as dag:
-    run_notebook = PapermillOperator(
-        task_id="run_notebook",
-        input_nb=os.path.join(os.path.dirname(INPUT_DIR), FILE_NAME),
-        output_nb=os.path.join(OUTPUT_DIR, FILE_NAME),
+    run_and_convert = PythonOperator(
+        task_id="run_and_convert",
+        python_callable=execute_notebook_and_convert,
     )
-
-    convert_to_html = BashOperator(
-        task_id="convert_to_html",
-        bash_command=f"jupyter nbconvert {os.path.join(OUTPUT_DIR, FILE_NAME)} --to html",
-    )
-
-    clean_up = BashOperator(
-        task_id="clean_up",
-        bash_command=f"rm {os.path.join(OUTPUT_DIR, FILE_NAME)}",
-    )
-
-    run_notebook >> convert_to_html >> clean_up
