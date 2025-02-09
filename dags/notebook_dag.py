24d6666343a30beebcd204c1c7311f59bb1949f7
Pim Mostert <mostert.pim@gmail.com>
2025-02-09 14:30:23 +0100

    Still got stuff working

--- dags/notebook_dag.py
+++ dags/notebook_dag.py
@@ -1,43 +1,52 @@
 import os
 import tempfile
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+from airflow import DAG, task
+from airflow.decorators import task
 import nbformat
 from nbconvert import HTMLExporter
 import papermill as pm
+import mlflow
 
 INPUT_DIR = os.path.realpath(__file__)
 OUTPUT_DIR = "/tmp"
 FILE_NAME = "notebook.ipynb"
 
 
-def execute_notebook_and_convert(**kwargs):
-    # Read the input notebook
-    INPUT_FILE = os.path.join(os.path.dirname(INPUT_DIR), FILE_NAME)
+@task(task_id="run_and_convert")
+def execute_notebook_and_convert(
+    mlflow_experiment_name,
+):
+    mlflow.set_experiment(mlflow_experiment_name)
 
-    with tempfile.NamedTemporaryFile(suffix=".ipynb") as fp:
-        pm.execute_notebook(
-            INPUT_FILE,
-            fp.name,
-        )
+    with mlflow.start_run() as run:
+        # Read the input notebook
+        INPUT_FILE = os.path.join(os.path.dirname(INPUT_DIR), FILE_NAME)
 
-        # Read notebook
-        executed_notebook = nbformat.read(fp, as_version=4)
+        with tempfile.NamedTemporaryFile(suffix=".ipynb") as fp:
+            executed_notebook = pm.execute_notebook(
+                INPUT_FILE,
+                fp.name,
+            )
 
-    # Convert notebook to HTML
-    html_exporter = HTMLExporter()
-    (html_body, _) = html_exporter.from_notebook_node(executed_notebook)
+            # Read notebook
+            # executed_notebook = nbformat.read(fp, as_version=4)
 
-    # Save the final HTML to disk
-    with open(os.path.join(OUTPUT_DIR, "output.html"), "w") as f:
-        f.write(html_body)
+        # Convert notebook to HTML
+        html_exporter = HTMLExporter()
+        html_body, _ = html_exporter.from_notebook_node(executed_notebook)
+
+        # Upload the final HTML to MLflow
+        with tempfile.TemporaryDirectory() as tempdir:
+            with open(os.path.join(tempdir, "output.html"), "w") as f:
+                f.write(html_body)
+
+                mlflow.log_artifact(f.name)
 
 
 with DAG(
     dag_id="run_notebook",
     description="Run notebook and export HTML",
 ) as dag:
-    run_and_convert = PythonOperator(
-        task_id="run_and_convert",
-        python_callable=execute_notebook_and_convert,
+    run_and_convert = execute_notebook_and_convert(
+        mlflow_experiment_name="run_notebook",
     )
