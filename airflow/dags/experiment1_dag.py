b762b19d12bd0a027c0c397106b6d8811a5e0e18
Pim Mostert <mostert.pim@gmail.com>
2025-02-09 15:08:47 +0100

    Cleaned up

--- airflow/dags/experiment1_dag.py
+++ airflow/dags/experiment1_dag.py
@@ -1,30 +1,24 @@
 import os
 import tempfile
-from airflow import DAG
-from airflow.decorators import task
+from airflow.decorators import task, dag
 from nbconvert import HTMLExporter
 import papermill as pm
 import mlflow
-
-INPUT_DIR = os.path.realpath(__file__)
-OUTPUT_DIR = "/tmp"
-FILE_NAME = "notebook.ipynb"
 
 
 @task(task_id="run_and_convert")
 def execute_notebook_and_convert(
     mlflow_experiment_name,
+    notebook_file,
 ):
     mlflow.set_experiment(mlflow_experiment_name)
 
     with mlflow.start_run() as run:
         os.environ["MLFLOW_RUN_ID"] = run.info.run_id
 
-        # Read the input notebook
-        INPUT_FILE = os.path.join(os.path.dirname(INPUT_DIR), FILE_NAME)
-
+        # Execute the notebook
         executed_notebook = pm.execute_notebook(
-            INPUT_FILE,
+            notebook_file,
             output_path=None,
             kernel_name="python3",
         )
@@ -33,7 +27,7 @@ def execute_notebook_and_convert(
         html_exporter = HTMLExporter()
         html_body, _ = html_exporter.from_notebook_node(executed_notebook)
 
-        # Upload the final HTML to MLflow
+        # Upload the HTML to MLflow
         with tempfile.TemporaryDirectory() as tempdir:
             with open(os.path.join(tempdir, "output.html"), "w") as f:
                 f.write(html_body)
@@ -41,10 +35,14 @@ def execute_notebook_and_convert(
                 mlflow.log_artifact(f.name)
 
 
-with DAG(
-    dag_id="run_notebook",
-    description="Run notebook and export HTML",
-) as dag:
-    run_and_convert = execute_notebook_and_convert(
-        mlflow_experiment_name="run_notebook",
+@dag(dag_id="experiment1", description="Experiment 1")
+def experiment1_dag():
+    execute_notebook_and_convert(
+        mlflow_experiment_name="Experiment 1",
+        notebook_file=os.path.join(
+            os.path.dirname(__file__), "experiment1.ipynb"
+        ),
     )
+
+
+experiment1_dag()
