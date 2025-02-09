b0542240fe70e22ba68227ff7e6548ff12e19a66
Pim Mostert <mostert.pim@gmail.com>
2025-02-09 21:36:09 +0100

    Cleanup

---
+++ dags/experiment1_dag.py
@@ -0,0 +1,17 @@
+import airflow
+from pathlib import Path
+
+from common.experiment import execute_and_upload
+
+experiment_id = "experiment1"
+
+
+@airflow.decorators.dag(dag_id=experiment_id)
+def dag():
+    execute_and_upload(
+        experiment_name=experiment_id,
+        notebook_path=Path(__file__).parents[0] / f"{experiment_id}.ipynb",
+    )
+
+
+dag()
