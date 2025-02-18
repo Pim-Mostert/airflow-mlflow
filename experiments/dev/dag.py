4ce2df56194eed1605819c3236d4607fda7e2d34
Pim Mostert <mostert.pim@gmail.com>
2025-02-18 14:22:36 +0100

    Rewrite as DAG context manager

--- experiments/dev/dag.py
+++ experiments/dev/dag.py
@@ -1,25 +1,21 @@
-import airflow
 from pathlib import Path
+from airflow import DAG
 from airflow.models.param import Param
 from common.experiment import execute_and_upload
 
 experiment_id = Path(__file__).parents[0].stem
 notebook_path = Path(__file__).parents[0] / f"{experiment_id}.py"
 
-
-@airflow.decorators.dag(
+# Create the DAG object using the context manager
+with DAG(
     dag_id=experiment_id,
     params={
         "run_name": Param(None, type=["null", "string"]),
         "name": Param("Pim", type="string"),
         "age": Param(35, type="integer"),
     },
-)
-def dag():
+) as dag:
     execute_and_upload(
         experiment_id=experiment_id,
         notebook_path=notebook_path,
     )
-
-
-dag()
