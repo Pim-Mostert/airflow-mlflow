f729b6273a798af7bab985c72fcfb306fe9b4079
Pim Mostert <mostert.pim@gmail.com>
2025-02-23 14:15:07 +0100

    Seem to have something working

--- dev/dag.py
+++ dev/dag.py
@@ -11,7 +11,7 @@ dag = create_experiment_dag(
     notebook_path,
     experiment_params={
         "name": Param(
-            default="Pim",
+            default="Pim & Truus",
             type="string",
             description="Your name.",
         ),
