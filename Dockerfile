3cba21222a9ba679d584fe297195b716df4e3e6e
Pim Mostert <mostert.pim@gmail.com>
2025-03-02 14:13:03 +0100

    Separate into dev/prod. Got dev working.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM apache/airflow:2.10.4-python3.12
 
-WORKDIR /pip
+ADD --chown=airflow:airflow packages /packages
 
-ADD --chown=airflow:airflow packages /home/airflow/packages
-
-RUN pip install -e /home/airflow/packages
+RUN pip install -e /packages
 
--- up.sh
+++ up.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Check if the first argument is -a (attach)
-if [ "$1" == "-a" ]; then
-    DETACH=""
-else
-    DETACH="-d"
-fi
+# if [ "$1" == "-a" ]; then
+#     DETACH=""
+# else
+#     DETACH="-d"
+# fi
 
-docker compose up $DETACH
+docker compose \
+    --env-file .env.dev \
+    -f docker-compose.base.yml \
+    -f docker-compose.dev.yml \
+    up \
+    --detach \
+    --build
