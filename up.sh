6b1985c04b83a5f640e10ae0b64bd75fcf3f01cc
Pim Mostert <mostert.pim@gmail.com>
2025-03-02 14:43:17 +0100

    Got prod working to

--- up.sh
+++ up.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# Check if the first argument is -a (attach)
-# if [ "$1" == "-a" ]; then
-#     DETACH=""
-# else
-#     DETACH="-d"
-# fi
+RUN_ENVIRONMENT=$1
 
-docker compose \
-    --env-file .env.dev \
-    -f docker-compose.base.yml \
-    -f docker-compose.dev.yml \
-    up \
-    --detach \
-    --build
+echo "Writing RUN_ENVIRONMENT=$RUN_ENVIRONMENT to .run_environment.txt"
+echo "$RUN_ENVIRONMENT" > .run_environment.txt
+
+# Check if the first argument is prod
+if [ "$RUN_ENVIRONMENT" == "prod" ]; then
+    docker compose \
+        --env-file .env.prod \
+        -f docker-compose.base.yml \
+        -f docker-compose.prod.yml \
+        up \
+        --detach
+else
+    docker compose \
+        --env-file .env.dev \
+        -f docker-compose.base.yml \
+        -f docker-compose.dev.yml \
+        up \
+        --detach \
+        --build
+fi
